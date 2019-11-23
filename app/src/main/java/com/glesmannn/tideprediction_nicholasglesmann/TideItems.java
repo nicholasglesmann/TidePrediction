b158ced5edbe6f8eed2b47dba8bf0f59f9a436ae
Nicholas Glesmann <nicholasglesmann@gmail.com>
2019-11-22 23:27:05 -0800

    Fixed a few issues

--- app/src/main/java/com/glesmannn/tideprediction_nicholasglesmann/TideItems.java
+++
@@ -1,7 +0,0 @@
-package com.glesmannn.tideprediction_nicholasglesmann;
-
-import java.util.ArrayList;
-
-public class TideItems extends ArrayList<TideItem> {
-    private static final long serialVersionUID = 1L;
-}
